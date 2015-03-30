cf03647bf60bac17b8eca7b27f0027e41454aead
Madhavi <mad.b2111@gmail.com>
2015-03-29 20:39:02 -0400

    Update DoctorDaoImpl.java

--- src/com/ehm/db/impl/DoctorDaoImpl.java
+++ src/com/ehm/db/impl/DoctorDaoImpl.java
@@ -50,7 +50,7 @@ public class DoctorDaoImpl implements DoctorDao {
 			paramList.add("%"+lName+"%");
 		}
 
-		
+
 		PreparedStatement ps = EHMDataConnect.getDataConn().prepareStatement(sqlBuf.toString());
 		// get customer data from database
 		int cnt = 1;
@@ -68,7 +68,12 @@ public class DoctorDaoImpl implements DoctorDao {
 			doct.setFirstName(result.getString("first_name"));
 			doct.setLastName(result.getString("last_name"));
 			doct.setSpecialization(result.getString("specialization_id"));
-
+			doct.setDoctorId(result.getInt("doctor_id"));
+			doct.setDesignation(result.getString("doctor_designation"));
+			doct.setVisitingDays(result.getString("doctor_visiting_days"));
+			doct.setVisitingHours(result.getString("doctor_visiting_Hours"));
+			doct.setEmailId(result.getString("doctor_email"));
+			doct.setPhoneNum(result.getString("doctor_phone_num"));
 			list.add(doct);
 		}
 
@@ -81,16 +86,14 @@ public class DoctorDaoImpl implements DoctorDao {
 		return null;
 	}
 
-	public Doctor getDoctorProfile(String doctoId) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+
+
 
 	public List<SelectItem> getSpecializationList() throws ClassNotFoundException, SQLException {
 
 		StringBuffer sqlBuf = new StringBuffer("select * from specializations ");
 
-		
+
 		PreparedStatement ps = EHMDataConnect.getDataConn().prepareStatement(sqlBuf.toString());
 		// get customer data from database
 		ResultSet result = ps.executeQuery();
@@ -105,7 +108,81 @@ public class DoctorDaoImpl implements DoctorDao {
 		}
 
 		return list;
-		
+
 	}
+
+	/*	public List<Doctor> getDoctorProfile(int doctorId) throws SQLException, ClassNotFoundException{
+
+		//		List<String> parameterList = new ArrayList<String>();
+
+		StringBuffer sqlBuf = new StringBuffer("select * from doctor ");
+		if (doctorId != 0) {
+			sqlBuf.append(" where doctor_ID = ?1 ");
+			//			parameterList.add(doctorId);
+		}
+
+
+		PreparedStatement ps = EHMDataConnect.getDataConn().prepareStatement(sqlBuf.toString());
+		// get customer data from database
+		//		int cnt = 1;
+		//		for (String param : parameterList) {
+		//			ps.setString(cnt, param);
+		//			cnt++;
+		//		}
+		ps.setInt(1, doctorId);
+
+		ResultSet result = ps.executeQuery();
+
+		List<Doctor> list = new ArrayList<Doctor>();
+
+		//		while (result.next()) {
+		Doctor doct = new Doctor();
+		doct.setDoctorId(result.getInt("doctor_ID"));
+		doct.setFirstName(result.getString("first_name"));
+		doct.setLastName(result.getString("last_name"));
+
+		doct.setQualification(result.getString("doctor_qualification"));
+		doct.setRegistrationNum(result.getString("doctor_reg_num"));
+		doct.setSpecialization(result.getString("specialization_id"));
+		doct.setDesignation(result.getString("doctor_designation"));
+		doct.setVisitingDays(result.getString("doctor_visiting_days"));
+		doct.setVisitingHours(result.getString("doctor_visiting_Hours"));
+		doct.setEmailId(result.getString("doctor_email"));
+		doct.setPhoneNum(result.getString("doctor_phone_num"));
+
+		list.add(doct);
+		//		}
+
+		return list;
+	}*/
+
+	public Doctor getDoctorProfile(int doctorId) throws ClassNotFoundException, SQLException {
+
+		Doctor doct = null;
+		StringBuffer sqlBuf = new StringBuffer("select * from doctor ");
+		sqlBuf.append(" where doctor_ID = ? ");
+
+		PreparedStatement ps = EHMDataConnect.getDataConn().prepareStatement(sqlBuf.toString());
+		ps.setInt(1, doctorId);
+
+		ResultSet result = ps.executeQuery();
+
+		while (result.next()) {
+			doct = new Doctor();
+			doct.setDoctorId(result.getInt("doctor_ID"));
+			doct.setFirstName(result.getString("first_name"));
+			doct.setLastName(result.getString("last_name"));
+			doct.setQualification(result.getString("doctor_qualification"));
+			doct.setRegistrationNum(result.getString("doctor_reg_num"));
+			doct.setSpecialization(result.getString("specialization_id"));
+			doct.setDesignation(result.getString("doctor_designation"));
+			doct.setVisitingDays(result.getString("doctor_visiting_days"));
+			doct.setVisitingHours(result.getString("doctor_visiting_Hours"));
+			doct.setEmailId(result.getString("doctor_email"));
+			doct.setPhoneNum(result.getString("doctor_phone_num"));
+		}
+		return doct;
+	}
+
 
 }
