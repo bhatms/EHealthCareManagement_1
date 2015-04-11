6213d61ac5378858f92e1e07fc2ffae075e6d7bb
teja151964 <dsajja@uncc.edu>
2015-04-10 21:12:52 -0400

    PatientQuery Java

--- src/com/ehm/db/model/PatientQuery.java
+++ src/com/ehm/db/model/PatientQuery.java
@@ -4,70 +4,69 @@ import java.util.Date;
 
 public class PatientQuery {
 
-	private int patient_id;
-	private int query_id;
-	private int sub_query_id;
-	private String query_category;
-	private String query_description;
-	private String query_status;
-	private Date query_date;
-	private int doctor_id;
-	private String doctors_reply;
-	
+	private int patientId;
+	private int queryId;
+	private int subQueryId;
+	private String queryCategory;
+	private String queryDescription;
+	private String queryStatus;
+	private Date queryDate;
+	private int doctorId;
+	private String doctorsReply;
 	public int getPatientId() {
-		return patient_id;
+		return patientId;
 	}
-	public void setPatientId(int patient_id) {
-		this.patient_id = patient_id;
+	public void setPatientId(int patientId) {
+		this.patientId = patientId;
 	}
 	public int getQueryId() {
-		return query_id;
+		return queryId;
 	}
-	public void setQueryId(int query_id) {
-		this.query_id = query_id;
-	}
-	public String getQueryDesc() {
-		return query_description;
-	}
-	public void setQueryDesc(String query_description) {
-		this.query_description = query_description;
-	}
-	public Date getQueryDate() {
-		return query_date;
-	}
-	public void setQueryDate(Date query_date) {
-		this.query_date = query_date;
+	public void setQueryId(int queryId) {
+		this.queryId = queryId;
 	}
 	public int getSubQueryId() {
-		return sub_query_id;
+		return subQueryId;
 	}
-	public void setSubQueryId(int sub_query_id) {
-		this.sub_query_id = sub_query_id;
+	public void setSubQueryId(int subQueryId) {
+		this.subQueryId = subQueryId;
 	}
 	public String getQueryCategory() {
-		return query_category;
+		return queryCategory;
 	}
-	public void setQuerycategory(String query_category) {
-		this.query_category = query_category;
+	public void setQueryCategory(String queryCategory) {
+		this.queryCategory = queryCategory;
+	}
+	public String getQueryDescription() {
+		return queryDescription;
+	}
+	public void setQueryDescription(String queryDescription) {
+		this.queryDescription = queryDescription;
 	}
 	public String getQueryStatus() {
-		return query_status;
+		return queryStatus;
 	}
-	public void setQueryStatus(String query_status) {
-		this.query_status = query_status;
+	public void setQueryStatus(String queryStatus) {
+		this.queryStatus = queryStatus;
+	}
+	public Date getQueryDate() {
+		return queryDate;
+	}
+	public void setQueryDate(Date queryDate) {
+		this.queryDate = queryDate;
 	}
 	public int getDoctorId() {
-		return doctor_id;
+		return doctorId;
 	}
-	public void setDoctorId(int doctor_id) {
-		this.doctor_id = doctor_id;
+	public void setDoctorId(int doctorId) {
+		this.doctorId = doctorId;
 	}
 	public String getDoctorsReply() {
-		return doctors_reply;
+		return doctorsReply;
 	}
-	public void setDoctorsReply(String doctors_reply) {
-		this.doctors_reply = doctors_reply;
+	public void setDoctorsReply(String doctorsReply) {
+		this.doctorsReply = doctorsReply;
 	}
-
+	
 	
 }
